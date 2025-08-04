56527eb5e933238a1500deeee908167fa5e869c7
Vansh <vanshmorwani1@gmail.com>
2025-08-05 00:11:31 +0530

    enabled cors for security from unauthorized requests

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,22 +1,33 @@
-import express from "express"
-import { dbConnect } from "./db"
-import router from "./route"
-const app = express()
-const port = 3000
-import { configDotenv } from "dotenv"
+import express from "express";
+import { dbConnect } from "./db";
+import router from "./route";
+const app = express();
+const port = 3000;
+import { configDotenv } from "dotenv";
+import cors from "cors";
+
+const frontend_origin = process.env.FRONTEND_URL;
+
+app.use(
+  cors({
+    origin: frontend_origin,
+    credentials: true,
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS','PATCH']
+  })
+);
 
 configDotenv();
 
 dbConnect();
 
-app.use(express.json())
+app.use(express.json());
 
-app.use('/api/v1',router)
+app.use("/api/v1", router);
 
-app.get('/',(req,res)=>{
-    res.send("Hello World")
-})
+app.get("/", (req, res) => {
+  res.send("Hello World");
+});
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+  console.log(`Example app listening on port ${port}`);
+});
