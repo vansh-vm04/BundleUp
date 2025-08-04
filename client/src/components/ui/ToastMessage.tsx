f94ded0f8b68e7ba32c978965f0af27cd4f0575c
Vansh <vanshmorwani1@gmail.com>
2025-08-05 00:35:06 +0530

    toast themes added

--- client/src/components/ui/ToastMessage.tsx
+++ client/src/components/ui/ToastMessage.tsx
@@ -1,8 +1,14 @@
 import { AnimatePresence, motion } from "framer-motion"
 import { useToast } from "../../hooks/useToast"
 
+const MessageTypes = {
+    'success' : 'bg-green-950 border-green-500',
+    'info' : 'bg-blue-950 border-blue-500',
+    'error' : 'bg-red-950 border-red-500'
+}
+
 const ToastMessage = () => {
-    const {isVisible,message} = useToast();
+    const {isVisible,message,type} = useToast();
   return (
     <AnimatePresence>
     {isVisible && (<motion.div
@@ -10,7 +16,7 @@ const ToastMessage = () => {
     animate={{opacity:1,y:0}}
     exit={{opacity:0,y:-20}}
     transition={{duration:0.1, ease:'easeIn'}}
-     className="bg-blue-950 border-1 border-blue-500 w-fit top-6 left-1/2 -translate-x-1/2 text-white rounded-md z-54 fixed h-fit px-8 py-4">
+     className={`${MessageTypes[type]} border-1  w-fit top-6 left-1/2 -translate-x-1/2 text-white rounded-md z-54 fixed h-fit px-8 py-4`}>
             <span className="text-gray-200">{message}</span>
     </motion.div>)}
     </AnimatePresence>
--- client/src/context/ToastContext.tsx
+++ client/src/context/ToastContext.tsx
@@ -1,15 +1,19 @@
 import { createContext, useState, type ReactNode } from "react"
 
+type ToastTheme = 'success' | 'error' | 'info'
+
 interface ToastType{
     isVisible:boolean,
-    toast: (key:string)=>void,
+    toast: (type:ToastTheme,message:string)=>void,
     message:string,
+    type: ToastTheme
 }
 
 const defaultToast:ToastType = {
     isVisible:false,
     toast:()=>{},
-    message:''
+    message:'',
+    type:'info'
 }
 
 export const ToastContext = createContext<ToastType>(defaultToast);
@@ -17,13 +21,15 @@ export const ToastContext = createContext<ToastType>(defaultToast);
 export const ToastProvider = ({children}:{children:ReactNode})=>{
     const [isVisible, setisVisible] = useState(false);
     const [message, setmessage] = useState('')
-    const toast = (message:string) =>{
+    const [type, settype] = useState<ToastTheme>('info')
+    const toast = (type:ToastTheme,message:string) =>{
+        settype(type)
         setmessage(message)
         setisVisible(true);
         setTimeout(()=>setisVisible(false),3000);
     }
     return (
-        <ToastContext.Provider value={{isVisible,message,toast}}>
+        <ToastContext.Provider value={{isVisible,message,toast,type}}>
             {children}
         </ToastContext.Provider>
     )
