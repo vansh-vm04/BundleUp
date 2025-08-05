44e4dbf9a554cc330578a0c52fc9a334d8cd3a3a
Vansh <vanshmorwani1@gmail.com>
2025-08-06 04:00:06 +0530

    usercard working with logout feature

--- client/src/components/icons/LogoutIcon.tsx
+++ client/src/components/icons/LogoutIcon.tsx
@@ -1,8 +1,12 @@
+interface Props{
+  onClick:()=>void
+}
 
-
-const LogoutIcon = () => {
+const LogoutIcon = (props:Props) => {
   return (
-    <svg className="icon icon-tabler icon-tabler-logout size-6 fill-white" fill="none" height="24" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24" width="24" xmlns="http://www.w3.org/2000/svg"><path d="M0 0h24v24H0z" fill="none" stroke="none"/><path d="M14 8v-2a2 2 0 0 0 -2 -2h-7a2 2 0 0 0 -2 2v12a2 2 0 0 0 2 2h7a2 2 0 0 0 2 -2v-2"/><path d="M7 12h14l-3 -3m0 6l3 -3"/></svg>
+    <div onClick={props.onClick} className="p-0.5 rounded-sm hover:cursor-pointer hover:bg-white/20">
+    <svg className="size-5 hover:cursor-pointer" fill="none" height="24" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24" width="24" xmlns="http://www.w3.org/2000/svg"><path d="M0 0h24v24H0z" fill="none" stroke="none"/><path d="M14 8v-2a2 2 0 0 0 -2 -2h-7a2 2 0 0 0 -2 2v12a2 2 0 0 0 2 2h7a2 2 0 0 0 2 -2v-2"/><path d="M7 12h14l-3 -3m0 6l3 -3"/></svg>
+    </div>
   )
 }
 
--- client/src/components/ui/UserCard.tsx
+++ client/src/components/ui/UserCard.tsx
@@ -1,18 +1,31 @@
-interface UserProps{
-  username:string,
-  theme?:"dark" | "light"
-}
+import { useEffect, useState } from "react";
+import { useAuth } from "../../hooks/useAuth"
+import LogoutIcon from "../icons/LogoutIcon"
 
 const ThemeType = {
-  "dark":"bg-black text-white rounded-md flex items-center w-[244px] justify-left px-6 gap-4 py-4",
+  "dark":"bg-black text-white rounded-md border-blue-900 border-1 flex items-center w-[264px] justify-left px-6 gap-4 py-4",
   "light":"bg-white text-black rounded-md flex items-center w-[244px] justify-left px-6 gap-4 py-4"
 }
 
-const UserCard = ({username}:UserProps) => {
+const UserCard = () => {
+  const [username, setusername] = useState<string>('User');
+  const {logOut,verify} = useAuth();
+  const getUser = async () =>{
+    const user = await verify();
+    if(user.loggedIn){
+      setusername(user.username);
+    }
+  }
+  
+  useEffect(() => {
+    getUser()
+  }, [])
+  
   return (
     <div className={ThemeType["dark"]}>
         <div className="bg-primary text-black text-xl rounded-full flex items-center justify-center p-4 w-12 h-12">{username.split('')[0].toUpperCase()}</div>
-        <span>{username.toLocaleUpperCase()}</span>
+        <span className="truncate w-[110px]">{username.toLocaleUpperCase()}</span>
+        <LogoutIcon onClick={()=>logOut()}/>
     </div>
   )
 }
