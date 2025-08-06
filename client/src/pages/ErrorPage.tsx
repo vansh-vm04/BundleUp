0552bac9e14c92a6a9a7be4299391090df288cdd
Vansh <vanshmorwani1@gmail.com>
2025-08-07 03:25:40 +0530

    error page added

---
+++ client/src/pages/ErrorPage.tsx
@@ -0,0 +1,10 @@
+
+const ErrorPage = () => {
+  return (
+    <div className='fixed flex flex-col items-center justify-center gap-4 bg-gray-900 z-[60] w-screen h-screen'>
+        <span className='text-white text-4xl font-semibold'>Page not found 🥲</span>
+    </div>
+  )
+}
+
+export default ErrorPage
